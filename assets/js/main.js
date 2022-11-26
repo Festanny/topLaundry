51b4840b21866f8f934324fb9f554ab228539307
Festanny <60522731+Festanny@users.noreply.github.com>
2022-11-26 21:45:12 +0400

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,24 @@
 // preloader
 window.onload = function () {
-    document.body.classList.add('loaded_hiding');
-    window.setTimeout(function () {
-        document.body.classList.add('loaded');
-        document.body.classList.remove('loaded_hiding');
-    }, 500);
-    document.body.style.overflow = 'auto';
-
-    // animation block
-    function onEntry(entry) {
-        entry.forEach(change => {
-            if (change.isIntersecting) {
-                change.target.classList.add('el-show');
-            }
-        });
-    }
-    let options = {
-        threshold: [0.3]
-    };
-    let observer = new IntersectionObserver(onEntry, options);
-    let elements = document.querySelectorAll('.el');
-
-    for (let elm of elements) {
-        observer.observe(elm);
-    }
-    
-    let placeholderBlock = document.querySelectorAll('.portfolio .info-block .block .img');
-    
-    placeholderBlock.forEach(el => {
-       el.style.display = 'block';
-    });
+    window.setTimeout(function() {
+        // document.body.classList.add('loaded');
+        $('.preloader').fadeOut()
+    }, 2100)
 };
 
 lottie.loadAnimation({
     container: document.getElementById('divided-heart'),
-    renderer : 'svg',
-    loop     : true,
-    autoplay : true,
-    path     : 'assets/json/preloader.json'
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'assets/json/preloader.json'
+});
+lottie.loadAnimation({
+    container: document.getElementById('logoAnimate'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/json/data.json'
 });
 
 // href
